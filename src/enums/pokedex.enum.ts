4b626179fcee5a96747013147eed8d0456e2dd52
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-23 10:04:50 +0100

    feat(sc-200): add into the PokemonServiceMethods enum a value for GetPokemons method

--- src/enums/pokedex.enum.ts
+++ src/enums/pokedex.enum.ts
@@ -6,6 +6,7 @@ export enum PokedexServices {
 export enum PokemonServiceMethods {
   GET_POKEMON_TYPES = 'GetPokemonTypes',
   GET_POKEMON_FORMS = 'GetPokemonForms',
+  GET_POKEMONS = 'GetPokemons',
 }
 
 export enum NatureServiceMethods {
